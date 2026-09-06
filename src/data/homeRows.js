3df06a530819cc3a82dd2bfaf7d3749b14005907
ZXH Official <zulqarnainhaider7116@gmail.com>
2026-09-06 08:24:57 +0500

    unzipgit

---
+++ src/data/homeRows.js
@@ -0,0 +1,4 @@
+export const HOME_ROWS=[
+ {title:'Trending Now',emoji:'🔥',key:'trending'}, {title:'Top Rated Movies',emoji:'⭐',key:'topMovies'}, {title:'Popular TV',emoji:'📺',key:'popularTV'}, {title:'Now Playing',emoji:'🎬',key:'nowPlaying'}, {title:'Upcoming',emoji:'🗓️',key:'upcoming'}
+];
+export const CATEGORIES=[['All','all'],['All Movies','movies'],['All Dramas','drama'],['Punjabi','punjabi'],['Action','action'],['Sci-Fi','scifi']];
